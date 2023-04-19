99886a091bd6280965f31cd661c29707a7af07c8
felix <felixvjr0@gmail.com>
2023-04-19 13:20:05 -0400

    REFACTORING

--- backend/src/main/java/com/felixvargas/PingPongController.java
+++ backend/src/main/java/com/felixvargas/PingPongController.java
@@ -9,8 +9,6 @@ public class PingPongController {
     private static int COUNTER = 0;
     record PingPong(String result) {}
 
-
-
     @GetMapping("/ping")
     public PingPong getPingPong(){
         return new PingPong("Pong %s".formatted(++COUNTER) );
