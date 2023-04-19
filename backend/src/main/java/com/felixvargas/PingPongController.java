fec6d9bc081fdba39d0ff479c99c99e1da2aabcc
felix <felixvjr0@gmail.com>
2023-04-19 00:04:46 -0400

    Testing CD2

--- backend/src/main/java/com/felixvargas/PingPongController.java
+++ backend/src/main/java/com/felixvargas/PingPongController.java
@@ -8,6 +8,9 @@ public class PingPongController {
 
     record PingPong(String message) {}
 
+
+
+
     @GetMapping("/ping")
     public PingPong getPingPong(){
         return new PingPong("Pong");
