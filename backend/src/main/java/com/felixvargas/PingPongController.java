bec4aa9360137a0cd0d7b2e357c29c16dac94222
felix <felixvjr0@gmail.com>
2023-04-19 17:27:17 -0400

    adding function in controller

--- backend/src/main/java/com/felixvargas/PingPongController.java
+++ backend/src/main/java/com/felixvargas/PingPongController.java
@@ -2,15 +2,31 @@ package com.felixvargas;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.w3c.dom.css.Counter;
 
 @RestController
 public class PingPongController {
 
     private static int COUNTER = 0;
+
+    public String counterNumber(){
+        String message = "counter is less than 5 Counter is currently: %s".formatted(COUNTER);
+        String message2 = "counter is greater than 5 Counter is currently: %s".formatted(COUNTER);
+
+        if(COUNTER < 5){
+            ++COUNTER;
+            return message;
+        }
+        else{
+            ++COUNTER;
+            return message2;
+        }
+    }
     record PingPong(String result) {}
+
 
     @GetMapping("/ping")
     public PingPong getPingPong(){
-        return new PingPong("Pong %s".formatted(++COUNTER) );
+        return new PingPong(counterNumber());
     }
 }
