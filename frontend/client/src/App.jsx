b2fbe57dbc876b0348b6b05f7b75344158845e09
felix <felixvjr0@gmail.com>
2023-04-21 01:09:56 -0400

    adding front end and CORS Acceptance

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,109 +1,52 @@
-import UserProfile from './UserProfile.jsx';
-import React, {useState, useEffect} from 'react'
+import { Button, Spinner, Text } from '@chakra-ui/react';
+import SideBarWithHeader from './shared/SideBar.jsx';
+import {useEffect, useState} from 'react';
+import {getCustomers} from './services/client.js';
 
-
-
-const users = [
-    {
-        name: 'Jasmin',
-        age: 22,
-        gender: "FEMALE"
-    },
-    {
-        name: 'Ana',
-        age: 32,
-        gender: "FEMALE"
-    },
-    {
-        name: 'Alex',
-        age: 45,
-        gender: "MALE"
-    },
-    {
-        name: 'Mike',
-        age: 82,
-        gender: "MALE"
-    },
-]
-
-const UserProfiles = ({users}) => (
-    <div>
-        {users.map((user, index) => (
-
-            <UserProfile key={index}
-                         name={user.name}
-                         age={user.age}
-                         gender={user.gender}
-                         imageNumber={index}
-            />
-        ))}
-
-    </div>
-)
-
-
-function App() {
-
-
-    const [counter, setCounter] = useState(0)
-    const [isLoading, setIsLoading] = useState(false)
-    const [showToast, setShowToast] = useState(false)
+const App = ()  => {
+    const [ customers, setCustomers] = useState([]);
+    const [ isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         setIsLoading(true)
         setTimeout(() => {
-            setIsLoading(false)
-        }, 3000)
-    }, [])
+            getCustomers().then((res) => {
+                setCustomers(res.data);
+            }).catch((err) => {
+                console.log(err);
+            }).finally(() => {
+                setIsLoading(false);
+            })
+        }, 3000);
+    }, []);
 
-    if(isLoading){
-        return <div className={"text-center"}>
-            <h3 className={" spinner-border text-primary"}></h3>
-        </div>
-    }
+        if(isLoading){
+          return ( <SideBarWithHeader>
+                <Spinner />
+            </SideBarWithHeader>)
+        }
 
-    // if(showToast) {
-    //     return <div className={"toast"} role={"alert"} aria-live={"assertive"} aria-atomic={"true"}>
-    //         <div className={"toast-header"}>
-    //             <img src={"..."} className={"rounded me-2"} alt={"..."}/>
-    //             <strong className={"me-auto"}>Bootstrap</strong>
-    //             <small>11 mins ago</small>
-    //             <button type={"button"} className={"btn-close"} data-bs-dismiss={"toast"} aria-label={"Close"}></button>
-    //         </div>
-    //         <div className={"toast-body"}>
-    //             Hello, world! This is a toast message.
-    //         </div>
-    //     </div>
-    // }
-
-
-    if(counter === -1){
-        setCounter(0)
-    }
-
+        if(customers.length <= 0){
+            return (
+                <SideBarWithHeader>
+                    <Text>No Customers Found!</Text>
+                </SideBarWithHeader>
+            )
+        }
 
     return (
-        <React.Fragment>
-
-    <button  className={"btn btn-primary"}
-             onClick={() => setCounter(counter + 1)} >
-
-        Increment Counter
-    </button>
-    <button className={"btn btn-warning"}
-            onClick={() => setCounter(counter - 1)}>
-
-        Decrement Counter
-    </button>
-    <button className={"btn btn-danger"}
-            onClick={() => setCounter(0)}>
-
-        Restart
-    </button>
-    <h1>{counter}</h1>
-    <UserProfiles users={users}/>
-        </React.Fragment>
+        <SideBarWithHeader>
+            {customers.map((customer, index) => {
+                return (
+                    <div>
+                        <Text>{customer.name}</Text>
+                        <Text>{customer.age}</Text>
+                        <Text>{customer.email}</Text>
+                    </div>
+                )
+            })}
+        </SideBarWithHeader>
     )
 }
 
-export default App
+export default App;
---
+++ frontend/client/src/services/client.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export const getCustomers = async() => {
+    try{
+       return await axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/v1/customer`)
+    } catch(error){
+        throw error;
+    }
+}
+
+export default getCustomers;
