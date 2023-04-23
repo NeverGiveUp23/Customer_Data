c6a06bcf86645fcd103ae7628a7e0919bd00d461
felix <felixvjr0@gmail.com>
2023-04-22 21:45:18 -0400

    ADD GENDER FEATURE

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -42,6 +42,7 @@ const App = ()  => {
                     <WrapItem key={index}>
                         <CardWithImage
                             {...customer}
+                            imageNumber={index}
                         />
                     </WrapItem>
 
